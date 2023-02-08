f73f1661f332083d371cb8bbf1ce74801de865b5
UMDstudent2024 <hadizalzalinew@gmail.com>
2023-02-08 16:38:59 -0500

    correct client version

--- client.py
+++ client.py
@@ -2,16 +2,22 @@ import socket
 import sys
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 3108))
-
 response = ""
-
-while response != "Bye":
-
+message = ""
+while True:
     #send command to server
-    message = input()
+    message = input(">> ")
+    response = ""
     s.send(message.encode('ascii'))
 
+    message = message.upper()
+    if (message == "QUIT"):
+        break
+    if (message == "SHUTDOWN"):
+        s.shutdown(1) #indicates client is done sending but will wait for a final receive
+        print(s.recv(2108).decode('ascii'))
+        break
     #read response from server
-    response = s.recv(2108).decode('ascii')
+    response = s.recv(6000).decode('ascii')
     print(response)
-
+s.close()
